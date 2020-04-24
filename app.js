0a22b77e20e1bd399f5de3378fa8535ec8b6d947
myhkas <mmyhkas@gmail.com>
2020-04-23 19:21:01 -0700

    first init

---
+++ app.js
@@ -0,0 +1,33 @@
+var questionsList = [
+  {
+    question: "Who are you?",
+    choices: ["a", "b", "c", "Myhkas"],
+    answer: "Myhkas",
+  },
+  {
+    question: "What are you?",
+    choices: ["a", "b", "c", "Nallas"],
+    answer: "Nallas",
+  },
+  {
+    question: "When are you?",
+    choices: ["a", "b", "c", "Now"],
+    answer: "Now",
+  },
+  {
+    question: "Why are you?",
+    choices: ["a", "b", "c", "Because"],
+    answer: "Because",
+  },
+  {
+    question: "Where are you",
+    choices: ["a", "b", "c", "Here"],
+    answer: "Here",
+  },
+];
+
+var score = 0;
+
+var container = document.querySelector(".container");
+
+for (var i = 0; i < questionsList.length; i++) {}
