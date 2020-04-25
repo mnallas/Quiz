ba964b53931c3e6603e7a114ca354b5dcb705c15
myhkas <mmyhkas@gmail.com>
2020-04-24 20:46:30 -0700

    choosing answers can now generate data-id, need to work on getting new prompts to appear

--- app.js
+++ app.js
@@ -1,33 +1,54 @@
-var questionsList = [
-  {
-    question: "Who are you?",
-    choices: ["a", "b", "c", "Myhkas"],
-    answer: "Myhkas",
-  },
-  {
-    question: "What are you?",
-    choices: ["a", "b", "c", "Nallas"],
-    answer: "Nallas",
-  },
-  {
-    question: "When are you?",
-    choices: ["a", "b", "c", "Now"],
-    answer: "Now",
-  },
-  {
-    question: "Why are you?",
-    choices: ["a", "b", "c", "Because"],
-    answer: "Because",
-  },
-  {
-    question: "Where are you",
-    choices: ["a", "b", "c", "Here"],
-    answer: "Here",
-  },
-];
+$(document).ready(function () {
+  var questionsList = [
+    {
+      question: "Who are you?",
+      choices: ["a", "b", "c", "Myhkas"],
+      answer: "Myhkas",
+    },
+    {
+      question: "What are you?",
+      choices: ["a", "b", "c", "Nallas"],
+      answer: "Nallas",
+    },
+    {
+      question: "When are you?",
+      choices: ["a", "b", "c", "Now"],
+      answer: "Now",
+    },
+    {
+      question: "Why are you?",
+      choices: ["a", "b", "c", "Because"],
+      answer: "Because",
+    },
+    {
+      question: "Where are you",
+      choices: ["a", "b", "c", "Here"],
+      answer: "Here",
+    },
+  ];
 
-var score = 0;
+  var score = 0;
+  var arrayQuestion = 0;
 
-var container = document.querySelector(".container");
+  $(document).on("click", "#start", function (e) {
+    e.preventDefault();
+    $("#question").text(questionsList[arrayQuestion].question);
+    for (var i = 0; i < questionsList[arrayQuestion].choices.length; i++) {
+      $("#choices").append(`
+      <div data-id = ${i} id="choice">${questionsList[arrayQuestion].choices[i]}</div>`);
+    }
+  });
 
-for (var i = 0; i < questionsList.length; i++) {}
+  $(document).on("click", "#choice", function () {
+    var chosen = $(this).attr("data-id");
+    console.log(questionsList[arrayQuestion].answer);
+    arrayQuestion++;
+  });
+
+  // function renderQuestion(arr) {
+  //   for (var i =0; i<arr.length;i++) {
+  //     $("#question").text(arr[i].question);
+  //   }
+  //   $("#choices").text(arr[arrayQuestion].choices);
+  // }
+});
